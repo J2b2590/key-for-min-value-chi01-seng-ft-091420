dccbe64b116197efd4c8250afb76281c458b8c18
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-03 22:16:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,16 @@ def key_for_min_value(name_hash)
 
     key_h = 0
     key_for_min_value = {}
-    while key_h < name_hash.each do
+    while key_h < name_hash.count do
       value_h = 0
       smallest_num = name_hash[key_h][value_h]
-      while value_h < name_hash[key_h].each do
+      while value_h < name_hash[key_h].count do
         if name_hash[key_h][value_h] < smallest_num
+          key_for_min_value << smallest_num
         end
         value_h += 1
       end
-    key_for_min_value << smallest_num
+
     key_h += 1
     end
     puts key_for_min_value

741b58c3c6a865cf70937713493cb2635e1d807e
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-03 22:33:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
 
 puts name_hash.each do |key, value|
 
-  min_val = 0
+  min_val += 1
 
-  puts "#{key}" if "#{value}" < min_val"#{value}"
+  puts "#{key}" if "#{value}" < min_val
 
   end
 

6c10df3ba2857e3d53ab518daedae0107f093337
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-03 22:22:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,15 @@
 
 def key_for_min_value(name_hash)
 
-
-  name_hash.each do |key, value|
+hash = ""
+  hash = name_hash.each do |key, value|
+    # row_count = 0
+    # key_h = []
+    # while row_count <
+    #
+    # end
     puts "#{key}"
-    puts "#{value}"  
+    puts "#{value}"
 
   end
 

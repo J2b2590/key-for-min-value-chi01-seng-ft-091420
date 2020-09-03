61fc25318315c853829ce30c0f31f2da706d2c7f
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-03 23:34:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |item, price|
-    if price < lowest_value || lowest_value == nil
+    if lowest_value == nil || price < lowest_value 
       lowest_value = price
       lowest_key = item
     end
